8d2654b3450b0a98761078fccd952aa5636369f9
Sangmin Choi <smchoi@altech.kr>
2026-01-14 15:27:04 +0900

    change google test server

--- update_google_xts_repository.sh
+++ update_google_xts_repository.sh
@@ -2,7 +2,6 @@ pssh -H "
 dev2-google01@192.168.2.201
 dev2-google03@192.168.2.203
 dev2-google04@192.168.2.204
-dev2-google-05@192.168.2.205
-dev2-google10@192.168.2.210
-dev2-google11@192.168.2.211
+dev2-google10@192.168.3.210
+dev2-google11@192.168.3.211
 " -i "cd ~/google_xts && git checkout -f && git clean -fd &&git pull"
