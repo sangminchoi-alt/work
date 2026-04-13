0cc190420e6eadeddd98ba71d11173ed36605aa8
Sangmin Choi <smchoi@altech.kr>
2026-04-13 10:55:01 +0900

    change google test pc

--- update_google_xts_repository.sh
+++ update_google_xts_repository.sh
@@ -1,6 +1,6 @@
 pssh -H "
 dev2-google01@192.168.2.201
-dev2-google03@192.168.2.203
+dev2-google02@192.168.2.202
 dev2-google04@192.168.2.204
 dev2-google10@192.168.3.210
 dev2-google11@192.168.3.211
